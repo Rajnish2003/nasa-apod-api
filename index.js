1ae7ccedee64c3566306bc206c91207ea346b52c
Rajnish2003 <104723302+Rajnish2003@users.noreply.github.com>
2023-05-07 07:52:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -97,6 +97,10 @@ app.post("/login", async (req, res) => {
     const { email, password } = req.body;
     const error = [];
     const user = await Collection.findOne({ email });
+    if(!user){
+        res.redirect('/login');
+        return;
+    }
     const validPass=await bcrypt.compare(password,user.password);
     if(validPass){
      req.session.user_id=user.email;
